5d958f7037c6a9081c1792ff0fde7d38cf6da8dd
riad <maksusbinislam@gmail.com>
2023-07-25 10:23:02 +0600

    #

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,76 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { request } from '../../Util/fetchAPI';
-import { Link } from 'react-router-dom';
-import { FaBed, FaSquareFull } from 'react-icons/fa';
-import classes from './featuredProperties.module.css';
-import person from '../assests/agent.png';
-import img1 from '../assests/2bed.jpeg';
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { request } from '../../Util/fetchAPI'
+import img from '../assests/2bed.jpeg'
+import person from '../assests/agent.png'
+import {Link } from 'react-router-dom'
+import {FaBed, FaSquareFull} from 'react-icons/fa'
+import classes from './featuredProperties.module.css'
 
 const FeaturedProperties = () => {
-  const [featuredProperties, setFeaturedProperties] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [featuredProperties, setFeaturedProperties] = useState([])
 
   useEffect(() => {
-    const fetchFeatured = async () => {
-      try {
-        const data = await request('/property/find/featured', 'GET');
-        setFeaturedProperties(data);
-        setLoading(false);
-      } catch (error) {
-        setError('Error fetching featured properties');
-        setLoading(false);
+    const fetchFeatured = async() => {
+      try{
+        const data = await request('property/find/featured', "GET")
+      
+        setFeaturedProperties(data)
+
+        console.log(data)
+
+      } catch (error){
+        console.error(error.message)
       }
-    };
 
-    fetchFeatured();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  console.log(featuredProperties)
+    }
+    fetchFeatured()
+    
+  }, [])
 
   return (
-    <div className={classes.container}>
+    <div className={classes.container} >
       <div className={classes.wrapper}>
         <div className={classes.titles}>
-          <h5>Properties You may Like</h5>
-          <h2>Our Featured Properties</h2>
+          <h5> Properties you may like</h5>
+          <h2> Our featured Properties</h2>
         </div>
-
         <div className={classes.featuredProperties}>
-          
             {featuredProperties.map((property) => (
               <div key={property._id} className={classes.featuredProperty}>
-                <Link to={`/PropertyDetail/${property._id}`} className={classes.imgContainer}>
-                  <img src={img1} alt="" />
+                <Link to={`/propertyDetail/${property._id}`} className={classes.imgContainer}> 
+                  <img src={img} alt="PropertyImage" />
                 </Link>
-                <div className={classes.priceAndOwner}>
-                  <span className={classes.price}>$ {property.price}</span>
-                  <img src={person} alt="" className={classes.owner} />
+                <div className={classes.details}>
+                  <div className={classes.priceAndOwner}>
+                    <span className={classes.price}> ${property.price}</span>
+                    <img src={person} alt="agent" className={classes.owner} />
+                  </div>
+                  <div className={classes.moreDetails}>
+                    <span>{property.beds} <FaBed className={classes.icon}/> </span>
+                    <span>{property.sqmeters} <FaSquareFull className={classes.icon}/> </span>
+                  </div>
+
+
                 </div>
-                <div className={classes.moreDetails}>
-                  <span>
-                    {property.beds} beds <FaBed className={classes.icon} />
-                  </span>
-                  <span>
-                    {property.sqmeters} square meters <FaSquareFull className={classes.icon} />
-                  </span>
-                </div>
-                <div className={classes.desc}>{property.desc}</div>
+                  
+
+
               </div>
-            ))
-            }
+            ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FeaturedProperties;
+export default FeaturedProperties
--- client/src/components/popularProperty/PopularProperty.jsx
+++ client/src/components/popularProperty/PopularProperty.jsx
@@ -4,28 +4,26 @@ import { Link } from 'react-router-dom';
 import mordernflat from '../assests/flat.jpg';
 import apartmenthouse from '../assests/apartment.jpg';
 import usahouse from '../assests/house.jpg';
-import FeaturedProperties from '../featuredProperties/FeaturedProperties';
 import { request } from '../../Util/fetchAPI';
 
 
 const PopularProperties = () => {
-  const [flatProperties, setflatProperties] = useState(0)
-  const [apartmentProperties, setapartmentProperties] = useState(0)
-  const [houseProperties, sethouseProperties] = useState(0)
+  const [numProperties, setNumProperties] = useState({})
+
 
   useEffect(() => {
-    const fetchPropertiesNumber = async() => {
+    const fetchNumberProperties = async() => {
       try {
          const data = await request('/property/find/types', 'GET')
 
-         setflatProperties(data.flat)
-         setapartmentProperties(data.apartment)
-         sethouseProperties(data.house)
+         setNumProperties(data)
+
+
       } catch (error) {
-        console.error(error)
+        console.error(error.message)
       }
     }
-    fetchPropertiesNumber()
+    fetchNumberProperties()
   }, [])
 
 
@@ -38,18 +36,18 @@ const PopularProperties = () => {
         </div>
         <div className={classes.properties}>
           <Link to={`/properties?type=flat&continent=0&priceRange=1`} className={classes.property}>
-            <img src={mordernflat} />
-            <div className={classes.quantity}>{flatProperties} properties</div>
+            <img src={mordernflat} alt="flat" />
+            <div className={classes.quantity}>{numProperties.flat} properties</div>
             <h5>Flat properties</h5>
           </Link>
           <Link to={`/properties?type=apartment&continent=1&priceRange=1`} className={classes.property}>
-            <img src={apartmenthouse} />
-            <div className={classes.quantity}>{apartmentProperties} properties</div>
+            <img src={apartmenthouse} alt="flat" />
+            <div className={classes.quantity}>{numProperties.apartment} properties</div>
             <h5>Apartment properties</h5>
           </Link>
           <Link to={`/properties?type=house&continent=2&priceRange=1`} className={classes.property}>
-            <img src={usahouse} />
-            <div className={classes.quantity}>{houseProperties} properties</div>
+            <img src={usahouse} alt="flat" />
+            <div className={classes.quantity}>{numProperties.house} properties</div>
             <h5>House properties</h5>
           </Link>
         </div>
